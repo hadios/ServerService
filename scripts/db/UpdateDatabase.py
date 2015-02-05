67764990e5c99e03d66f2067b16ef0f67fbed4f7
damienhadios <damienhadios@gmail.com>
2015-02-05 17:00:20 +0800

    Update script to add item to database and update it already exist.

--- scripts/db/UpdateDatabase.py
+++ scripts/db/UpdateDatabase.py
@@ -7,25 +7,18 @@ FILE_FORMAT = {
 				'edition' 			: 2,
 				'region'			: 3,
 				'priceSingapore'	: 4,
-				'priceMalaysia'		: 5 };		
+				'priceMalaysia'		: 5 };
 
-def ListAllItemInDatabase (database):
-	posts = database.post;
+DATABASE_NAME 	= 'test_database';
+COLLECTION_NAME = 'productlist';				
+
+def ListAllItemInDatabase (database, collectionName):
+	posts = database[collectionName];
 	
 	for post in posts.find():
 		print(post);
 		
-def Main ():
-	# Connect to default mongo client
-	client = MongoClient();
-	db = client.test_database;
-
-	# Print out existing information in database
-	print(db);
-
-	# Open up collection
-	posts = db.productlist;
-	
+def UpdateAndAddProductInfoFromList(collectionToAddTo, filename):
 	# Open file and read line by line
 	with open(PRODUCTFILE_NAME) as file:
 		line = file.readline;
@@ -36,29 +29,47 @@ def Main ():
 				skip = False;
 				continue;
 			
-			print(currentLine);
+			#print(currentLine);
 			elements = currentLine.split(',');
 		
+			# Create the product information
+			# Add sample document
+			sampleItem = { "itemName" 		: elements[FILE_FORMAT['itemName']],
+						   "Edition"		: elements[FILE_FORMAT['edition']],
+						   "Region" 		: elements[FILE_FORMAT['region']],
+						   "Price($SG)" 	: elements[FILE_FORMAT['priceSingapore']],
+						   "Price($MY)" 	: elements[FILE_FORMAT['priceMalaysia']] };
+		
+			#print(sampleItem);
+		
 			# Check if item is already in database
+			findResult = collectionToAddTo.find_one({"itemName": sampleItem['itemName']});
+			
+			if (findResult != None):
 				# If it is, update the price
-				
+				collectionToAddTo.update({"itemName": sampleItem['itemName']}, 
+										 {"$set": 
+											{"Price($SG)": sampleItem['Price($SG)'], 
+											 "Price($MY)": sampleItem['Price($MY)']}
+										 });
+			else:
 				# Else, insert the data
-		
-		
-	'''
-	# Add sample document
-	sampleItem = { "itemName" 	: "Ace Combat Assault Horizon Legacy",
-				   "Edition"	: "Standard",
-				   "Region" 	: "NTSC(US)",
-				   "Price" 		: "$39.00" };
-				   
+				collectionToAddTo.insert(sampleItem);
 	
-	post_id = posts.insert(sampleItem);
-	print(post_id);
-	'''
-
-	ListAllItemInDatabase(db);
+def Main ():
+	# Connect to default mongo client
+	client = MongoClient();
 	
+	# Get database
+	db = client[DATABASE_NAME];
+	
+	# Create new collection
+	productCollection = db[COLLECTION_NAME];
+	productCollection.remove();
+	
+	UpdateAndAddProductInfoFromList(productCollection, PRODUCTFILE_NAME);
+	#ListAllItemInDatabase(db, COLLECTION_NAME);
+	print("Total items: " + str(productCollection.count()));
 	
 #------------------------------------------------------------------------------
 Main();
